6d3e8686739eb0022c5e8ec1ce176d1ae95bb85d
Atreyo073 <107910400+Atreyo073@users.noreply.github.com>
2022-11-15 20:43:09 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,28 @@
+username=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome "+username+"!";
+function add_room()
+{
+      roomname=document.getElementById("room_name").value ;
+      firebase.database().ref("/").child(roomname).update({
+            purpose:"Adding RoomName"
+      });
+      localStorage.setItem("room_name",roomname);
+      window.location="kwitter_room.html";
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+console.log(Room_names);
+redirect_To_Rooms="<div class='room_name' id="+Room_names+"onclick='check(this.id)'>#"+Room_names+"</div><hr>";
+document.getElementById("output").innerHTML +=redirect_To_Rooms;
+//End code
+});});}
+getData();
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
